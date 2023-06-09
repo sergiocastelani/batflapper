a4230d839d3c26d0bc1d4f07a8e38020b15c8032
Sergio C Castelani Jr <sergio.castelani@gmail.com>
2023-06-09 12:59:37 -0300

    Timer for the "close ad" button

--- Build/html/assets/ads.js
+++ Build/html/assets/ads.js
@@ -5,6 +5,30 @@ function PrepareAd()
 function ShowAd()
 {
     document.getElementById("adOverlay").style.visibility = "visible";
+
+    //close button timer
+    let closeButton = document.getElementById("closeIcon");
+    let closeTimerText = document.getElementById("closeTimerText");
+    closeButton.style.display = "none";
+    closeTimerText.style.display = "block";
+    closeTimerText.innerText = "5 s";
+    let initialTime = (new Date()).getTime();
+    let closeIntervalId = setInterval(() => 
+    {
+        const now = (new Date()).getTime();
+        const timeLeft = 5000 - (now - initialTime);
+
+        if (timeLeft <= 0)
+        {
+            closeButton.style.display = "block";
+            closeTimerText.style.display = "none";
+            clearInterval(closeIntervalId);
+        }
+        else
+        {
+            closeTimerText.innerText = `${(Math.ceil(timeLeft / 1000)).toFixed(0)} s`;
+        }
+    }, 300);
 }
 
 function closeAd()
